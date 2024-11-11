c86d71f806fd1d77759cb3e0dfd4716d691e9936
sigunamu226 <sigunamu226@gmail.com>
2024-11-12 02:34:07 +0900

    Pass items state to InputBody and ListBody

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import { Header } from "@/components/organisms/Header";
 import { InputBody } from "@/components/organisms/List/InputBody";
 import { ListBody } from "@/components/organisms/List/ListBody";
+import { useState } from "react";
 
 export default function Page() {
+  const [items, setItems] = useState<string[]>([]);
+
   return (
     <>
       <Header />
       <div className="container md:mx-auto">
-        <InputBody />
-        <ListBody />
+        <InputBody items={items} setItems={setItems} />
+        <ListBody items={items} setItems={setItems} />
       </div>
     </>
   );
