13c403d1968d823324f7fa9d9f6fc92f17bb290b
sigunamu226 <sigunamu226@gmail.com>
2024-11-30 18:58:44 +0900

    Change directory name and file name

--- src/app/todolist/page.tsx
+++ src/app/todolist/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+import { auth } from "@/common/firebase";
 import { Header } from "@/components/organisms/Header";
 import { InputBody } from "@/components/organisms/List/InputBody";
 import { ListBody } from "@/components/organisms/List/ListBody";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export default function Page() {
   const [items, setItems] = useState<string[]>([]);
+
+  const user = useMemo(() => {
+    return auth.currentUser;
+  }, []);
+
+  if (!user) {
+    return <></>;
+  }
 
   return (
     <>
       <Header />
       <div className="container md:mx-auto">
-        <InputBody items={items} setItems={setItems} />
+        <InputBody items={items} setItems={setItems} user={user} />
         <ListBody items={items} setItems={setItems} />
       </div>
     </>
--- src/services/todolist.ts
+++ src/services/todolist.ts
@@ -1,8 +1,12 @@
-export const addItem = (
+export const initializeItems = {};
+
+export const save = (
   inputValue: string,
+  userId: string,
   items: string[],
   setItems: React.Dispatch<React.SetStateAction<string[]>>
 ): void => {
   if (!inputValue) return;
+
   setItems([...items, inputValue]);
 };
