7e27dd570359926e703d2872c5242aa278b784e9
sigunamu226 <sigunamu226@gmail.com>
2025-01-13 18:05:22 +0900

    Fix screen height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-base-background bg-custom-gradient h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-base-background bg-custom-gradient h-svh`}
       >
         <NextUIProvider>{children}</NextUIProvider>
       </body>
--- src/app/todolist/layout.tsx
+++ src/app/todolist/layout.tsx
@@ -20,5 +20,5 @@ export default function Layout({
     return unsubscribe;
   }, [router]);
 
-  return <div className="h-svh">{children}</div>;
+  return <>{children}</>;
 }
