3ccbc2bf81e15516b9bd8c9830570d7d49d8a395
InoueHayato226 <inouehayato@rootteam.co.jp>
2025-02-09 00:57:55 +0900

    Fix enviroments variables

--- src/common/firebase_admin.ts
+++ src/common/firebase_admin.ts
@@ -1,11 +1,16 @@
-import { initializeApp } from "firebase-admin";
-import { cert, getApp, getApps, ServiceAccount } from "firebase-admin/app";
+import {
+  cert,
+  getApp,
+  getApps,
+  initializeApp,
+  ServiceAccount,
+} from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
 const firebaseAdminConfig: ServiceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  projectId: process.env.FIREBASE_ADMIN_PROJECT_ID!,
+  privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL!,
 };
 
 const appName = "firebase-admin";
